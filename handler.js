fb50cbefc1e4d701cfbcdeddf3ba86ea6266a211
jameschristou <james.christou@gmail.com>
2019-09-16 21:17:36 +1000

    Adding more colours and returning random colour

--- handler.js
+++ handler.js
@@ -1,13 +1,52 @@
 exports.handler = async (event) => {
-  let game = {
+  let colours = [
+    {
       type: 'Colour',
       colour: 'Green',
       questionText: 'What colour is this?'
-  };
+    },
+    {
+      type: 'Colour',
+      colour: 'Red',
+      questionText: 'What colour is this?'
+    },
+    {
+      type: 'Colour',
+      colour: 'Blue',
+      questionText: 'What colour is this?'
+    },
+    {
+      type: 'Colour',
+      colour: 'Yellow',
+      questionText: 'What colour is this?'
+    },
+    {
+      type: 'Colour',
+      colour: 'Pink',
+      questionText: 'What colour is this?'
+    },
+    {
+      type: 'Colour',
+      colour: 'Black',
+      questionText: 'What colour is this?'
+    },
+    {
+      type: 'Colour',
+      colour: 'White',
+      questionText: 'What colour is this?'
+    },
+    {
+      type: 'Colour',
+      colour: 'Orange',
+      questionText: 'What colour is this?'
+    }
+  ];
+
+  let randomColour = colours[Math.trunc(Math.random()*colours.length)];
   
   const response = {
       statusCode: 200,
-      body: JSON.stringify(game),
+      body: JSON.stringify(randomColour),
       isBase64Encoded: false
   };
   
