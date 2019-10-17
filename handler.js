24422dfc6e174265d97c7c9ded20449ec9db48ee
jameschristou <james.christou@gmail.com>
2019-10-18 08:00:40 +1100

    Including colour code to set background colour on test

--- handler.js
+++ handler.js
@@ -3,41 +3,49 @@ exports.handler = async (event) => {
     {
       type: 'Colour',
       colour: 'Green',
+      colourCode: '#00FF00',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'Red',
+      colourCode: '#FF0000',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'Blue',
+      colourCode: '#0000FF',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'Yellow',
+      colourCode:'#FFFF00',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'Pink',
+      colourCode: '#FF00FF',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'Black',
+      colourCode: '#000000',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'White',
+      colourCode: '#FFFFFF',
       questionText: 'What colour is this?'
     },
     {
       type: 'Colour',
       colour: 'Orange',
+      colourCode: '#F39C12',
       questionText: 'What colour is this?'
     }
   ];
@@ -47,7 +55,10 @@ exports.handler = async (event) => {
   const response = {
       statusCode: 200,
       body: JSON.stringify(randomColour),
-      isBase64Encoded: false
+      isBase64Encoded: false,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
   };
   
   return response;
